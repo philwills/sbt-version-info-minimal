183d14772d5ba009e09897f79ba8d480ba9caa50
Philip Wills <philip.wills@guardian.co.uk>
2012-07-24 15:56:26 +0100

    Minimal example of sbt-version-info plugin usage
    Ready for testing with Jenkins

---
+++ project/project/Plugins.scala
@@ -0,0 +1,6 @@
+import sbt._
+
+object Plugins extends Build {
+  lazy val plugins = Project("plugins", file("."))
+    .dependsOn(uri("git://github.com/guardian/sbt-version-info-plugin.git#2.2"))
+}
